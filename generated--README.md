dbc35954fbfbf11c7b923b222f9d68364b0e9653
forestDean <113769986+forestDean@users.noreply.github.com>
2023-05-28 13:51:22 +0100

    written to generated--README.md

--- generated--README.md
+++ generated--README.md
@@ -1,4 +1,4 @@
-# Apollo Moon
+# Apollo Moon Launch
   [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js?ref=badge_shield)
   ## Description
   Blah
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,20 +2,20 @@
 // function generateMarkdown(data) {
 //const generateMarkdown = (data) => {
   //const generateMarkdown = () =>
-  export function generateMarkdown () {
-//   return `# ${data.title}
-//   [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js?ref=badge_shield)
-//   ## Description
-//   ${data.description}
-//   ## Installation
-//   ## Usage
-//   ## API
-//   ## Roadmap
-//   ## Credits
-//   ## License
+  export function generateMarkdown (data) {
+  return `# ${data.title}
+  [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js?ref=badge_shield)
+  ## Description
+  ${data.description}
+  ## Installation
+  ## Usage
+  ## API
+  ## Roadmap
+  ## Credits
+  ## License
 
-// `;
-console.log('generateMarkdown is selfAware')
+`;
+//console.log('generateMarkdown is selfAware')
 }
 
 //module.exports = generateMarkdown; // this is CommonJS not ES6
