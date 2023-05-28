b8230e01bd247c61434c3129372ebc9238de1328
forestDean <113769986+forestDean@users.noreply.github.com>
2023-05-28 11:56:13 +0100

    Successfully written to log.txt

--- test.js
+++ test.js
@@ -9,18 +9,37 @@ import * as path from 'path';
 const questions = [
     {
         type: 'input',
-        message: 'What is your Project name?',
-        name: 'projectname',
+        message: 'What is your project title?',
+        name: 'title',
+        // filter: (title) => {
+        //     title.toLowerCase().split(' ')
+        //     // // .split(' ')
+        //     //.map(word) => {
+        //     .map(function(word) {
+        //         //return (word.charAt(0).toUpperCase() + word.slice(1)); 
+        //         return word.replace(word[0], word[0].toUpperCase());
+        //         //return title.join(' ');  
+        //     });
+        //      return title.join(' '); 
+
+        // }
     },
     {
         type: 'input',
-        message: 'What is your location?',
-        name: 'location',
-    // },
-    // {
-    //     type: 'editor',
-    //     message: 'Write a summary about yourself?',
-    //     name: 'summary',
+        message: 'Write a brief description.',
+        name: 'description',
+    },
+    {
+        type: 'editor',
+        message: 'Write a summary about yourself?',
+        name: 'summary',
+        waitUserInput: true,
+    },
+    {
+        type: 'input',
+        message: 'License',
+        name: 'licence',
+        default: 'MIT',
     // },
     // {
     //     type: 'input',
@@ -70,28 +89,40 @@ const questions = [
 
 // function to write README file
 function writeToFile(data) {
-    console.log(data.projectname);
-    console.log(data.location);
-    // fs.appendFile('log.txt',
-    //     `${data.projectname}`,
-    //      (err) =>
-    //      err ? console.error(err) : console.log('Saved!')
-    //     )
+    console.log(data.title);
+    console.log(data.description);
+    console.log(data.summary);
+    console.log(data.licence);
+    fs.writeFile('log.text', data.title,
+    
+    // fs.writeFile('generateREADME.md', data.title,
+       (err) =>
+         err ? console.error(err) : console.log('Saved!')
+        )
+
 }
 
 // function to initialize program
 //async function init() {
 //const getData = await 
 function init() {
-inquirer.prompt(questions)
-.then(data => 
-//await(() =>
-    // console.log(data.projectname);
-    // console.log(data.location);
-    writeToFile(data)
-    //return data
-);
-};
+inquirer
+    .prompt(questions)
+    .then(data => 
+    //await(() =>
+        //fs.writeFile('generateMarkdown.js', generateMarkdown(data))
+        writeToFile(data)
+        //return data
+    )
+    .then(() => console.log('Successfully written to generateMarkdown.js'))
+    .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+    });
+ };
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,22 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+const generateMarkdown = (data) =>
+  //return 
+  `# ${data.title}
+  [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FSBoudrias%2FInquirer.js?ref=badge_shield)
+  ## Description
+  ${data.description}
+  ## Installation
+  ## Usage
+  ## API
+  ## Roadmap
+  ## Credits
+  ## License
+
 
 `;
-}
+//}
 
 module.exports = generateMarkdown;
+
+fs.writeFile('generated--README.md', generateMarkdown);
