cec0b7682b95e1a4630b0387ce6de8f2445a5b7b
forestDean <113769986+forestDean@users.noreply.github.com>
2023-05-28 18:47:46 +0100

    licenceObject

--- index.js
+++ index.js
@@ -2,28 +2,46 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 //import path  from 'path ';
 import * as path from 'path';
-// const fs = require("fs");
-// const path = require('path');
-// const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown");
-const generateMarkdown = import("./utils/generateMarkdown")
+import {generateMarkdown} from './utils/generateMarkdown.js'; //named import
+import {licence} from './utils/licenceObject.js'; //named import
 
 // array of questions for user
 const questions = [
     {
         type: 'input',
-        message: 'What is your Project name?',
-        name: 'projectname',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What is your location?',
-    //     name: 'location',
-    // },
-    // {
-    //     type: 'editor',
-    //     message: 'Write a summary about yourself?',
-    //     name: 'summary',
+        message: 'What is your project title?',
+        name: 'title',
+        // filter: (title) => {
+        //     title.toLowerCase().split(' ')
+        //     // // .split(' ')
+        //     //.map(word) => {
+        //     .map(function(word) {
+        //         //return (word.charAt(0).toUpperCase() + word.slice(1)); 
+        //         return word.replace(word[0], word[0].toUpperCase());
+        //         //return title.join(' ');  
+        //     });
+        //      return title.join(' '); 
+
+        // }
+    },
+    {
+        type: 'input',
+        message: 'Write a brief description.',
+        name: 'description',
+    },
+    {
+        type: 'editor',
+        message: 'Write a summary about yourself?',
+        name: 'summary',
+        waitUserInput: true,
+    },
+    {
+        type: 'list',
+        message: 'License',
+        name: 'licence',
+        //pageSize: 14,
+        default: 'MIT',
+        choices: licence,
     // },
     // {
     //     type: 'input',
@@ -72,25 +90,36 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-    fs.appendFile('log.txt',
-        `${data.projectname}`,
-         (err) =>
-         err ? console.error(err) : console.log('Saved!')
+function writeToFile(data) {
+    let markup = generateMarkdown(data);
+    fs.writeFile('generated--README.md', markup,
+       (err) =>
+         err ? console.error(err) : console.log('Successfully written to generated--README.md')
         )
 }
 
 // function to initialize program
 //function init() {
-const init = () => {
-//node index.js
-inquirer.prompt([
-     ...questions
-])
-.then(() =>
-    writeToFile(data)
-)
-};
-
+    const init = async () => {
+        console.log('Welcome to the README generator...');
+        //const data =
+    
+    inquirer
+        .prompt(questions)
+        .then(data => 
+        //await(() =>
+            writeToFile(data)
+        )
+        .then(() => console.log('Successfully written to writeToFile()'))
+        .catch((error) => {
+            if (error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+            } else {
+              // Something else went wrong
+            }
+        });
+        
+     };
+     
 // function call to initialize program
 init();
