f185dd8f2bf0e0d46492ade978a2cf487b659ac2
forestDean <113769986+forestDean@users.noreply.github.com>
2023-05-29 13:49:52 +0100

    complete licenceObject

--- utils/licenceObject.js
+++ utils/licenceObject.js
@@ -1,63 +1,86 @@
 export const licence = [
     {
         'name':  'None',
-        'value': 'https://img.shields.io/badge/licence-NONE-lightgrey'
+        'value': { 'icon': 'https://img.shields.io/badge/licence-NONE-lightgrey',
+                   'info': 'No Licence' 
+        }
     },
     {
         'name':  'MIT License',
         'value': { 'icon': 'https://img.shields.io/badge/License-MIT-blue.svg',
-                   'info': 'MIT License info'
+                   'info': 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
         }
     },
     {
         'name':  'Apache License 2.0',
-        'value': 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+                   'info': 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code'  
+        }
     },
     {
         'name':  'GNU General Public License v3.0',
-        'value': 'https://img.shields.io/badge/License-GPLv3-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+                   'info': 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'  
+        }
     },
     {
         'name':  'BSD 2-Clause "Simplified" License',
-        'value': 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg',
+                   'info': 'A permissive license that comes in two variants, the BSD 2-Clause and BSD 3-Clause. Both have very minute differences to the MIT license.'  
+        }
     },
     {
         'name':  'BSD 3-Clause "New" or "Revised" License',
-        'value': 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+                   'info': 'A permissive license similar to the BSD 2-Clause License, but with a 3rd clause that prohibits others from using the name of the copyright holder or its contributors to promote derived products without written consent.'  
+        }
     },
     {
         'name':  'Boost Software License 1.0',
-        'value': 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg',
+                   'info': 'A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'  
+        }
     },
     {
         'name':  'Creative Commons Zero v1.0 Universal',
-        'value': 'https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg',
+                   'info': 'The Creative Commons CC0 Public Domain Dedication waives copyright interest in a work you have created and dedicates it to the world-wide public domain. Use CC0 to opt out of copyright entirely and ensure your work has the widest reach. As with the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is very similar to the Unlicense.'  
+        }
     },
     {
         'name':  'Eclipse Public License 2.0',
-        'value': 'https://img.shields.io/badge/License-EPL_2.0-red.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-EPL_2.0-red.svg',
+                   'info': 'This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones.'  
+        }
     },
     {
         'name':  'GNU Affero General Public License v3.0',
-        'value': 'https://img.shields.io/badge/License-AGPL_v3-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-AGPL_v3-blue.svg',
+                   'info': 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.'  
+        }
     },
     {
         'name':  'GNU General Public License v2.0',
-        'value': 'https://img.shields.io/badge/License-GPL_v2-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-GPL_v2-blue.svg',
+                   'info': 'The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.'  
+        }
     },
     {
         'name':  'GNU Lesser General Public License v2.1',
-        'value': 'https://img.shields.io/badge/License-LGPL_v2.1-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-LGPL_v2.1-blue.svg',
+                   'info': 'Primarily used for software libraries, the GNU LGPL requires that derived works be licensed under the same license, but works that only link to it do not fall under this restriction. There are two commonly used versions of the GNU LGPL.'  
+        }
     },
     {
         'name':  'Mozilla Public License 2.0',
-        'value': 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg',
+                   'info': 'Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.'  
+        }
     },
     {
         'name':  'The Unlicense',
-        'value': 'https://img.shields.io/badge/License-Unlicense-blue.svg'
+        'value': { 'icon': 'https://img.shields.io/badge/License-Unlicense-blue.svg',
+                   'info': 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.'  
+        }
     }
 ]
-
-
-
